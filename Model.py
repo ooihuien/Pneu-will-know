5fb7508b4e756847e8b7ff2307467c37cf77f96c
ooihuien <61439145+ooihuien@users.noreply.github.com>
2022-01-18 19:05:25 +0800

    Update Model.py

--- Model.py
+++ Model.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-import opencv-python as cv2
+import opencv_python as cv2
 from skimage import exposure
 import pandas as pd
 import time
